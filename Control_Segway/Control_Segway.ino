0610d1153aa71478e7ab22bc6b5c20e027a449ef
andresbmth <andres-bmth@hotmail.com>
2015-11-20 19:48:43 -0500

    Codigo final, falta pulir

--- Control_Segway/Control_Segway.ino
+++ Control_Segway/Control_Segway.ino
@@ -1,13 +1,17 @@
 #include <Wire.h> 
 
 #define MPU 0x68  //Direccion I2C de la IMU
-#define LEFT 10   
-#define RIGHT 11
+#define LEFT 11   
+#define RIGHT 10
+#define ENABLE 9
+int vel;
 int16_t GY; // la MPU arroja valores de 16 bits
 
 void setup() {
   pinMode(LEFT,OUTPUT);
   pinMode(RIGHT,OUTPUT);
+  pinMode(ENABLE,OUTPUT);
+   pinMode(A0,INPUT);
   Wire.begin();
   Wire.beginTransmission(MPU);  // Se inicia la comunicacion por I2C con el MPU
   Wire.write(0x6B);   
@@ -28,17 +32,22 @@ void loop() {
    Wire.endTransmission(false);
    Wire.requestFrom(MPU,2,true); //A partir del 0x45, se pide el dato del giroscopio en y
    GY=Wire.read()<<8|Wire.read();  
-   Serial.print("GY: "); 
-   Serial.print(GY);  
-   Serial.print("\n");
+   //Serial.print("GY: "); 
+   //Serial.print(GY);  
+   //Serial.print("\n");
+   
    
    if(GY>0){
-     analogWrite(LEFT,HIGH);
-     analogWrite(RIGHT,LOW);
-   }else{
-     analogWrite(LEFT,LOW);
-     analogWrite(RIGHT,HIGH);   
+     digitalWrite(LEFT,LOW);
+     digitalWrite(RIGHT,HIGH);
+     vel=map(GY,0,10000,255,90);
+     analogWrite(ENABLE,vel);
+   }else if(GY<=-0){
+     digitalWrite(LEFT,HIGH);
+     digitalWrite(RIGHT,LOW);
+     vel=map(GY,-10000,0,70,255);
+     analogWrite(ENABLE,vel);
    }
    
-   delay(333); //Tiempo entre muestra, 0.01  
+   delay(10); //Tiempo entre muestra, 0.01  
 }
